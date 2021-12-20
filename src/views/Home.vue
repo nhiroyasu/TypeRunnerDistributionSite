c3b4240ad0ece5cff34251828c30713bff8a81c7
N-HIROYASU <nhiro1109pri@gmail.com>
2021-12-20 23:20:04 +0900

    コンポーネント分離

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,15 +7,7 @@
           <div id="app-name">{{ appName }}</div>
         </div>
         <div id="app-message">{{ message }}</div>
-        <a
-          :href="donwloadLink"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="install-button"
-        >
-          <img src="@/assets/save.svg" alt="#" />
-          <div>Install for macOS</div>
-        </a>
+        <install-button />
       </div>
       <div class="side-images">
         <img src="@/assets/background-item1.png" alt="#" />
@@ -26,14 +18,15 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import InstallButton from "@/components/InstallButton.vue";
 
 export default defineComponent({
   name: "Home",
-  components: {},
+  components: {
+    InstallButton,
+  },
   data() {
     return {
-      donwloadLink:
-        "https://firebasestorage.googleapis.com/v0/b/typerunner.appspot.com/o/distribution%2FTypeRunnerInstaller%20version1.0.0.dmg?alt=media&token=dc96004f-d57f-4db6-925d-55caeaf823b4",
       appName: "TypeRunner",
       message: "Let's count your typing.",
     };
@@ -130,39 +123,6 @@ export default defineComponent({
   #app-message {
     font-size: 1.4em;
     color: #ffffff;
-  }
-}
-
-.install-button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 48px;
-  background: #ffffff;
-  border-radius: 8px;
-  font-size: 1.2rem;
-  font-weight: bold;
-  padding: 0px 32px;
-  cursor: pointer;
-  transition: all 0.3s linear;
-  text-decoration: none;
-  color: #1eb5a4;
-
-  @include if_md_greater {
-    margin-top: 48px;
-  }
-  @include if_md_less {
-    margin-top: 16px;
-  }
-
-  img {
-    width: 32px;
-    height: 32px;
-    margin-right: 8px;
-  }
-
-  &:hover {
-    box-shadow: 6px 6px 0px rgba($color: #007e70, $alpha: 0.8);
   }
 }
 </style>
