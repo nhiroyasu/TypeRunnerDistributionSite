a922c95904d34392197659585d79306e31b34cbd
N-HIROYASU <nhiro1109pri@gmail.com>
2021-12-20 23:14:25 +0900

    レイアウト修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="home">
-    <img src="@/assets/logo.png" alt="#" width="256" />
-    <div id="app-name">TypeRunner</div>
-    <a
-      :href="donwloadLink"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="install-button"
-    >
-      <img src="@/assets/save.svg" alt="#" />
-      <div>Install for macOS</div>
-    </a>
+  <div class="root">
+    <div class="top">
+      <div class="contents">
+        <div class="app-name-wrapper">
+          <img src="@/assets/logo.png" alt="#" width="256" />
+          <div id="app-name">{{ appName }}</div>
+        </div>
+        <div id="app-message">{{ message }}</div>
+        <a
+          :href="donwloadLink"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="install-button"
+        >
+          <img src="@/assets/save.svg" alt="#" />
+          <div>Install for macOS</div>
+        </a>
+      </div>
+      <div class="side-images">
+        <img src="@/assets/background-item1.png" alt="#" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,43 +34,126 @@ export default defineComponent({
     return {
       donwloadLink:
         "https://firebasestorage.googleapis.com/v0/b/typerunner.appspot.com/o/distribution%2FTypeRunnerInstaller%20version1.0.0.dmg?alt=media&token=dc96004f-d57f-4db6-925d-55caeaf823b4",
+      appName: "TypeRunner",
+      message: "Let's count your typing.",
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.home {
-  width: 100vw;
+@import "@/styles/responsive.scss";
+
+.root {
+  padding: 0px 64px;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
+  display: grid;
   align-items: center;
+  justify-items: center;
+
+  @include phone_only {
+    padding: 0;
+  }
+}
+.top {
+  width: 100%;
+  max-width: 1600px;
+  height: 100vh;
+
+  display: grid;
   justify-content: center;
-  color: #1eb5a4;
+  align-content: center;
+  grid-template-areas:
+    "content"
+    "side-image";
+  grid-template-columns: 1fr;
+
+  @include if_md_greater {
+    grid-template-areas: "content side-image";
+    grid-template-columns: 1fr 1fr;
+  }
 }
 
-#app-name {
-  font-size: 4rem;
-  font-weight: bold;
+.side-images {
+  grid-area: side-image;
+  justify-self: end;
+  width: 100%;
+  height: 100%;
+
+  @include if_md_greater {
+    margin-top: 0px;
+  }
+  @include if_md_less {
+    margin-top: 32px;
+  }
+
+  img {
+    width: 80%;
+  }
+}
+
+.contents {
+  grid-area: content;
+  height: 100%;
+  color: #1eb5a4;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @include if_md_greater {
+    font-size: 2rem;
+    align-items: flex-start;
+  }
+  @include if_md_less {
+    font-size: 1rem;
+    align-items: center;
+  }
+
+  .app-name-wrapper {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+
+    img {
+      $icon-size: 96px;
+      height: $icon-size;
+      width: $icon-size;
+    }
+  }
+
+  #app-name {
+    font-size: 2em;
+    font-weight: bold;
+  }
+
+  #app-message {
+    font-size: 1.4em;
+    color: #ffffff;
+  }
 }
 
 .install-button {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 64px;
+  height: 48px;
   background: #ffffff;
-  border-radius: 16px;
-  font-size: 24px;
+  border-radius: 8px;
+  font-size: 1.2rem;
   font-weight: bold;
   padding: 0px 32px;
-  margin: 32px;
   cursor: pointer;
-  box-shadow: 0px 3px 6px rgba($color: #000000, $alpha: 0.16);
   transition: all 0.3s linear;
   text-decoration: none;
   color: #1eb5a4;
+
+  @include if_md_greater {
+    margin-top: 48px;
+  }
+  @include if_md_less {
+    margin-top: 16px;
+  }
 
   img {
     width: 32px;
@@ -69,7 +162,7 @@ export default defineComponent({
   }
 
   &:hover {
-    box-shadow: 0px 4px 8px rgba($color: #000000, $alpha: 0.32);
+    box-shadow: 6px 6px 0px rgba($color: #007e70, $alpha: 0.8);
   }
 }
 </style>
